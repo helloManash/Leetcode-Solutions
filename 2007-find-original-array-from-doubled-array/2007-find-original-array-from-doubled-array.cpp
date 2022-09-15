f6c49f855a7acb7fb479e2616b13d155cab084c0
ManashX <77092566+ManashX@users.noreply.github.com>
2022-09-15 19:50:18 +0530

    Time: 564 ms (74.04%), Space: 116.5 MB (98.79%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -4,18 +4,31 @@ public:
         int n = changed.size();
         if(n % 2 == 1) return {};
         sort(changed.begin(), changed.end());
+        
         vector<int>ans;
-        map<int, int>mp;
-        for(int i =  0; i < n; i++){
-            mp[changed[i]]++;
-        }
         for(int i = 0; i < n; i++){
-            if(mp[changed[i]] == 0) continue;
-            if(mp[changed[i] * 2] == 0) return {};
-            ans.push_back(changed[i]);
-            mp[changed[i]]--;
-            mp[changed[i] * 2]--;
+            int x = changed[i];
+            if(x < 0) continue;
+            
+            auto idx = lower_bound(begin(changed) + i + 1, end(changed), x * 2);
+            
+            if(idx == changed.end() ){
+                return vector<int>();
+            }
+            else{
+                if( *idx == 2 * x){
+                    
+                    changed[i] = -1;
+                    *idx = -1;
+                    ans.push_back(x);
+                }
+                else{
+                    return {};
+                }
+            }
         }
+        
+        
         return ans;
     }
 };
