87f8a545d33a1d9a1901c379dc12afb208d3e944
ManashX <77092566+ManashX@users.noreply.github.com>
2022-09-17 18:23:46 +0530

    Time: 60 ms (18.40%), Space: 19.2 MB (57.74%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    void swap(int * a, int * b){
-        int temp = *a;
-        *a = *b;
-        *b = temp;
-    }
+    
     void moveZeroes(vector<int>& nums) {
-        int i = 0, k = 0;
-        if(nums.size() == 0 || nums.size() == 1) return;
-        while(k < (int)nums.size()){
-            if(nums[k] != 0){
-                if(nums[i] == 0){
-                    swap(&nums[k], &nums[i]);
-                    
-                }
-                i++;
+        int shift = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(!nums[i]){
+                shift++;
+                continue;
             }
-            else{
-                if(nums[i] != 0){
-                    i++;
-                }
+            nums[i - shift] = nums[i];
+            if(shift){
+                nums[i] = 0;
             }
-            k++;
         }
+        
     }
 };
