4f505e75f6038040c49e28642926274c369c7cad
ManashX <77092566+ManashX@users.noreply.github.com>
2022-10-28 22:21:26 +0530

    Time: 236 ms (9.61%), Space: 37.5 MB (5.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<map<char,int>,vector<string>>mpp; 
-        map<char,int>mpp2;
+        map<map<char,int>,vector<string>>mpp;  
         for(string &s : strs)
         {
+            map<char,int>mpp2;
             for(char &ch : s)
                 mpp2[ch]++;
             mpp[mpp2].push_back(s);
-            mpp2.clear();
         }
         vector<vector<string>> ans;
         for(auto &x : mpp)
