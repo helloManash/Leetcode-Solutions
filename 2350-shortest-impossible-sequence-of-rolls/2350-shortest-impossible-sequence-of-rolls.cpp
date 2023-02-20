f1ae95d806aa5870e71f83ef6fc097212657295e
ManashX <77092566+ManashX@users.noreply.github.com>
2023-02-20 19:18:53 +0530

    Time: 327 ms (30.39%), Space: 119.4 MB (13.26%) - LeetHub

---
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int shortestSequence(vector<int>& rolls, int k) {
+        set<int>s;
+        int ans = 0;
+        for(int i = 0; i < rolls.size(); i++){
+            s.insert(rolls[i]);
+            if(s.size() == k){
+                ans++;
+                s.clear();
+            }
+        }
+        return ans + 1;
+    }
+};
